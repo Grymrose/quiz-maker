660a2639bc21a8e60a7489cfd4e20ae3df08c289
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2023-11-22 15:48:45 -0800

    Added empty file QuestionPool.cpp for references. Waiting for further development.

---
+++ src/QuestionPool.cpp
@@ -0,0 +1,17 @@
+#include "../header/QuestionPool.hpp"
+
+QuestionPool::QuestionPool(){
+
+}
+
+QuestionPool::~QuestionPool(){
+    
+}
+
+Question QuestionPool::GetRandomQuestion(){
+
+}
+
+Question QuestionPool::GetQuestionByID(unsigned QuestionID){
+
+}
