6f7e00f7a66d55befd11f11a902c159ad0480950
Grymrose <NelsonTT1337@gmail.com>
2023-12-03 22:40:46 -0800

    Adjust login function

--- src/User.cpp
+++ src/User.cpp
@@ -4,5 +4,5 @@ User::User(std::string username, std::string password, bool isInstructor)
         : username(username), password(password), isInstructor(isInstructor) {}
 
 bool User::Login(std::string usernameEntered, std::string passwordEntered) {
-    (username == usernameEntered && password == passwordEntered) ? return true : return false;
+    return (username == usernameEntered && password == passwordEntered) ? true : false;
 }
