a6702dea2a24f138439a816f76d2698ecace567c
Grymrose <NelsonTT1337@gmail.com>
2023-12-04 01:08:29 -0800

    Syntax error with AddQuestion().

--- src/Quiz.cpp
+++ src/Quiz.cpp
@@ -19,34 +19,6 @@ void Quiz::setAttempts(int attempts) {
     Attempts = attempts;
 }
 
-void scheduleQuiz(Quiz& quiz);{
-    struct Time {
-        int Month;
-        int Day;
-        int Year;
-        int Hour;
-        int Minute;
-        int Second;
-
-        Time():Time(0,0,0,0,0,0){ 
-
-        } 
-
-        Time(int month, int day, int year, int hour, int minute, int second){
-            Month = month;
-            Day = day;
-            Year = year;
-            Hour = hour;
-            Minute = minute;
-            Second = second;
-        };
-
-        void ReadTime(){
-
-        }
-
-        void DisplayTime{
-            cout << Month << "," << Day << ","<< Year << " " << Hour << ":" << Minute << ":"<< Second;
-        }
-    };
+void Quiz::AddQuestion(std::shared_ptr<Question> question) {
+    Questions.push_back(question);
 }
