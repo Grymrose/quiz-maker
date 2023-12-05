cae3636d5085bd42deec345d33bbcae9c5bdaeda
Grymrose <NelsonTT1337@gmail.com>
2023-12-05 09:51:16 -0800

    New tests

--- test/TestQuizSession.cpp
+++ test/TestQuizSession.cpp
@@ -12,27 +12,37 @@
 TEST(TestQuizSession, SubmitAnswers) {
     Quiz aQuiz(1, "Science Quiz");
     auto mcqQuestion = std::make_shared<QuestionMCQ>(1, 10, "What is the capital of France?");
+    mcqQuestion->AddPossibleAnswer("Paris")->Correctness = true;
+    mcqQuestion->AddPossibleAnswer("London");
+    mcqQuestion->AddPossibleAnswer("Berlin");
+    mcqQuestion->AddPossibleAnswer("Madrid");
+    mcqQuestion->AddPossibleAnswer("Rome");
+    mcqQuestion->AddPossibleAnswer("True")->Correctness = true;
+    mcqQuestion->AddPossibleAnswer("False");
     aQuiz.AddQuestion(mcqQuestion);
-    auto tfQuestion = std::make_shared<QuestionTF>(2, 5, "Is the Earth round?");
-    aQuiz.AddQuestion(tfQuestion);
 
     std::shared_ptr<Quiz> quizPtr = std::make_shared<Quiz>(aQuiz);
     QuizSession quizSession(quizPtr);
 
+    // The correct answers are "Paris" and "True"
     std::vector<std::string> answers = {"Paris", "True"};
     quizSession.SubmitAnswers(answers);
 
-    EXPECT_EQ(quizSession.GetScore(), 15);
+    // The expected score is calculated based on the percentage of correct answers
+    // The question has 2 correct answers out of 6 total options, so the expected score is 5.
+    EXPECT_EQ(quizSession.GetScore(), 5);
 }
 
 TEST(TestQuizSession, GetScore) {
     Quiz aQuiz(1, "Math Quiz");
     auto frqQuestion = std::make_shared<QuestionFRQ>(1, 15, "Solve for x: 2x + 5 = 15");
+    frqQuestion->SetCorrectAnswer("5");
     aQuiz.AddQuestion(frqQuestion);
 
     std::shared_ptr<Quiz> quizPtr = std::make_shared<Quiz>(aQuiz);
     QuizSession quizSession(quizPtr);
 
+    // For a free-response question, the entire score is awarded if the answer is correct
     std::vector<std::string> answers = {"5"};
     quizSession.SubmitAnswers(answers);
 
