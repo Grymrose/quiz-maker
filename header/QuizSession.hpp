243ee1c0795a3a8d6dd987dbb29bb21a91caab2f
Grymrose <NelsonTT1337@gmail.com>
2023-12-05 23:16:29 -0800

    Removing shared pointers

--- header/QuizSession.hpp
+++ header/QuizSession.hpp
@@ -3,34 +3,33 @@
 
 #include <string>
 #include <vector>
-#include <memory>  // Include for shared_ptr
 #include "../header/Quiz.hpp"
 #include "../header/QuestionMCQ.hpp"
 #include "../header/QuestionTF.hpp"
 #include "../header/QuestionFRQ.hpp"
 
 class QuizSession {
-private:
-    std::shared_ptr<Quiz> quiz;  // Use shared_ptr for managing Quiz object
-    std::vector<std::string> answers;
-    int score;
-    int timeElapsed;
+    private:
+        Quiz* quiz;
+        std::vector<std::string> answers;
+        int score;
+        int timeElapsed;
 
-public:
-    // Constructor takes a shared_ptr to Quiz
-    QuizSession(std::shared_ptr<Quiz> quiz);
+    public:
+        // Constructor takes a pointer to Quiz
+        QuizSession(Quiz* quiz);
 
-    // Submit student answers to the quiz
-    void SubmitAnswers(const std::vector<std::string>& inputAnswers);
+        // Submit student answers to the quiz
+        void SubmitAnswers(const std::vector<std::string>& inputAnswers);
 
-    // Get the score achieved in the quiz
-    int GetScore() const;
+        // Get the score achieved in the quiz
+        int GetScore() const;
 
-    // Get the time elapsed during the quiz
-    int GetTimeElapsed() const;
+        // Get the time elapsed during the quiz
+        int GetTimeElapsed() const;
 
-    // Set the time elapsed during the quiz
-    void SetTimeElapsed(int time);
+        // Set the time elapsed during the quiz
+        void SetTimeElapsed(int time);
 };
 
 #endif
--- src/QuizSession.cpp
+++ src/QuizSession.cpp
@@ -1,14 +1,14 @@
 #include "../header/QuizSession.hpp"
 
 // Constructor implementation
-QuizSession::QuizSession(std::shared_ptr<Quiz> quiz) : quiz(quiz), score(0), timeElapsed(0) {}
+QuizSession::QuizSession(Quiz* quiz) : quiz(quiz), score(0), timeElapsed(0) {}
 
 // Submit student answers to the quiz
 void QuizSession::SubmitAnswers(const std::vector<std::string>& inputAnswers) {
-    const std::vector<std::shared_ptr<Question>>& questions = quiz->GetQuestions();
+    const std::vector<Question*>& questions = quiz->GetQuestions();
 
     for (size_t i = 0; i < questions.size(); ++i) {
-        std::shared_ptr<Question> currentQuestion = questions[i];
+        Question* currentQuestion = questions[i];
         score += currentQuestion->ScoreQuestion(inputAnswers[i]);
     }
 }
